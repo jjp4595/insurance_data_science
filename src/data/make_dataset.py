a568c61cbb449c8f993d624653629fd08034606c
Jordan Pannell <jordanjamespannell@gmail.com>
2022-09-06 13:07:09 +0100

    make_dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -2,26 +2,26 @@
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
-from data.home_insurance import housingdata
+from src.data.home_insurance import Housingdata
+from constants import PROJECT_DIR, LOGGING_DIR
+from src.utils.logging import configure_logging
+
+logger = logging.getLogger(__name__)
+debug = False
 
 
-def main(input_filepath, output_filepath):
+def main():
     """Runs data processing scripts to turn raw data from (../raw) into
     cleaned data ready to be analyzed (saved in ../processed).
     """
-    logger = logging.getLogger(__name__)
+    configure_logging(LOGGING_DIR / "modelling-data-collation.log", debug, logger)
+
     logger.info("making final data set from raw data")
-    df = housingdata._run()
-    df.to_csv(Path("../data/processed/processed_data.csv"))
+    df = Housingdata()._run()
+    df.to_csv(Path(PROJECT_DIR / "data/processed/processed_data.csv"))
 
 
 if __name__ == "__main__":
-    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
     load_dotenv(find_dotenv())
