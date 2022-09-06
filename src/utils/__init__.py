12c67a8b158e7c0f1f3ab249132527f88417c489
Jordan Pannell <jordanjamespannell@gmail.com>
2022-09-06 13:05:44 +0100

    fix init

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,2 +1,4 @@
-from insurance_data_science.utils import logging
-from insurance_data_science.utils import decorators
+from src.utils import logging
+from src.utils import decorators
+
+__all__ = ["logging", "decorators"]
