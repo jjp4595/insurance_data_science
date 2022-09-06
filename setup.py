0e9ae14d9704a5898e368a661eaa41ad31529f9c
Jordan Pannell <jordanjamespannell@gmail.com>
2022-09-06 13:06:08 +0100

    formatter

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='A data science project to predict future lapsed cases in a Kaggle dataset of Home insurance claims.',
-    author='Jordan Pannell',
-    license='MIT',
+    version="0.1.0",
+    description="A data science project to predict future lapsed cases in a Kaggle dataset of Home insurance claims.",  # noqa
+    author="Jordan Pannell",
+    license="MIT",
 )
