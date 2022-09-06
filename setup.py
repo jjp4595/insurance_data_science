8712ccc731a9f8b0c6174db6e775b9af25c92848
Jordan Pannell <jordanjamespannell@gmail.com>
2022-09-06 10:27:05 +0100

    rest of repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A data science project to predict future lapsed cases in a Kaggle dataset of Home insurance claims.',
+    author='Jordan Pannell',
+    license='MIT',
+)
