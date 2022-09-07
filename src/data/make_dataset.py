380928f9702276b1de743b0619da785d58bd111c
Jordan Pannell <jordanjamespannell@gmail.com>
2022-09-07 08:46:37 +0100

    renaming functions

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
 from src.data.home_insurance import Housingdata
 from constants import PROJECT_DIR, LOGGING_DIR
 from src.utils.logging import configure_logging
@@ -10,20 +9,23 @@ logger = logging.getLogger(__name__)
 debug = False
 
 
-def main():
-    """Runs data processing scripts to turn raw data from (../raw) into
-    cleaned data ready to be analyzed (saved in ../processed).
+def build_dataset():
+    """Runs data pre-processing scripts to turn raw data from (../raw) into
+    cleaned data ready to be analyzed (saved in ../interim).
     """
     configure_logging(LOGGING_DIR / "modelling-data-collation.log", debug, logger)
 
-    logger.info("making final data set from raw data")
+    logger.info("Beginning data pre-processing")
     df = Housingdata()._run()
-    df.to_csv(Path(PROJECT_DIR / "data/processed/processed_data.csv"))
+    df.to_csv(Path(PROJECT_DIR / "data/interim/cleaned_data.csv"))
+    logger.info("Data pre-processing finished")
+    return None
 
 
 if __name__ == "__main__":
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+    try:
+        build_dataset()
+    except Exception as ex:
+        logger.exception(str(ex))
+        print(str(ex))
+        raise ex
