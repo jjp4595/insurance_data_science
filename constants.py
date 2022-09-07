dd108ea9a391bad9cca696523ac0eee1cc445657
Jordan Pannell <jordanjamespannell@gmail.com>
2022-09-07 09:35:24 +0100

    updating PROJECT_DIR

--- constants.py
+++ constants.py
@@ -1,7 +1,8 @@
 import tempfile
 from pathlib import Path
+import os
 
 # paths
-PROJECT_DIR: Path = Path().resolve()
+PROJECT_DIR: Path = Path(os.path.dirname(os.path.abspath(__file__)))
 LOGGING_DIR: Path = Path(PROJECT_DIR / "logs/")
 TMP_DIR: Path = Path(tempfile.gettempdir())
